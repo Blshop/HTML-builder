3825849f8ae2bd45dc35d2ab2367e6f42411b498
Blshop <envworking@gmail.com>
2025-01-19 12:37:58 +0300

    feat:01 task solved

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fileStream = require('fs')
+const path = require('path')
+const fileName = 'text.txt'
+const pathToFile = path.join(__dirname, fileName)
+
+const rs = fileStream.createReadStream(pathToFile, 'utf8')
+rs.on('data', data => console.log(data))
+rs.on('end', () => console.log('Reading completed.'))
+rs.on('error', err => console.error('An error occurred while reading:', err))
