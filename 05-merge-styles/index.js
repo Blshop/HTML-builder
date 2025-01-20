522e5fc4db9f7df3476dbb6769b1a8a2f8a13b29
Blshop <envworking@gmail.com>
2025-01-20 21:35:27 +0300

    fix: correct function name

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,10 +7,10 @@ const stylesFolder = 'styles'
 const resultFilePath = path.join(__dirname, projectFolder, resultFileName)
 const stylesPath = path.resolve(__dirname, stylesFolder)
 
-async function copyDir() {
+async function compileCSS() {
     const ws = fs.createWriteStream(resultFilePath, 'utf8')
-    const toCopyFiles = await fsPromises.readdir(stylesPath)
-    for (let file of toCopyFiles) {
+    const toCopystyles = await fsPromises.readdir(stylesPath)
+    for (let file of toCopystyles) {
         if (path.extname(file) === '.css') {
             const styleFilePath = path.join(stylesPath, file)
             const rs = fs.createReadStream(styleFilePath, 'utf8')
@@ -23,4 +23,4 @@ async function copyDir() {
         }
     }
 }
-copyDir()
+compileCSS()
